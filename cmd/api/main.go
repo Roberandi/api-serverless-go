3babe3c04598a1749891c14038ad460cc71072bb
Roberandi <roberandid10@gmail.com>
2026-06-07 11:06:37 -0400

    Agregando CI/CD pipeline de GitHub Actions 1.3

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -44,6 +44,18 @@ func init() {
 	// 3. Configurar el Servidor Web (Router)
 	router := gin.Default()
 
+	// Agregar middleware de CORS manual
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 	// En AWS Lambda, la única carpeta donde tenemos permiso de escritura es /tmp
 	os.MkdirAll("/tmp/uploads", os.ModePerm)
 	router.Static("/uploads", "/tmp/uploads")
