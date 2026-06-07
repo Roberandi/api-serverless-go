3848815f094e66b2d42f2eeb0eb981de4a122249
Roberandi <roberandid10@gmail.com>
2026-06-07 11:13:07 -0400

    Corrigiendo nombres de recursos a V2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
-# 1. Crear un Rol de IAM para que Lambda tenga permisos de ejecutarse y escribir Logs
+# 1. Crear un Rol de IAM para que Lambda tenga permisos de ejecutarse
 resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_api_role"
+  name = "serverless_api_role_v2" # <-- CAMBIO AQUÍ
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -20,16 +20,13 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
 
 # 2. Definir la Función Lambda
 resource "aws_lambda_function" "api_lambda" {
-  function_name    = "apigolang-serverless"
-  # Asumimos que GitHub Actions empaquetará tu código de Go en un zip llamado bootstrap.zip
+  function_name    = "apigolang-serverless-v2" # <-- CAMBIO AQUÍ
   filename         = "../bootstrap.zip"
   handler          = "bootstrap"
   source_code_hash = filebase64sha256("../bootstrap.zip")
   role             = aws_iam_role.lambda_exec.arn
-  # Usamos el entorno más moderno y rápido de AWS para Go
   runtime          = "provided.al2"
 
-  # Variables de entorno que viviran dentro de AWS Lambda
   environment {
     variables = {
       DATABASE_URL = var.database_url
@@ -39,9 +36,9 @@ resource "aws_lambda_function" "api_lambda" {
   }
 }
 
-# 3. Crear el API Gateway (El Recepcionista web)
+# 3. Crear el API Gateway
 resource "aws_apigatewayv2_api" "http_api" {
-  name          = "apigolang-http-api"
+  name          = "apigolang-http-api-v2" # <-- CAMBIO AQUÍ
   protocol_type = "HTTP"
 }
 
@@ -58,14 +55,13 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   integration_method = "POST"
 }
 
-# Conectar todas las rutas a nuestro código en Go
 resource "aws_apigatewayv2_route" "default_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "ANY /{proxy+}"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
-# 4. Dar permiso al API Gateway para que despierte a tu Lambda
+# 4. Dar permiso al API Gateway
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
